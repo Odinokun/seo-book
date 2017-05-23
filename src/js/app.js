645ca5fc6c512a9fe45cac63983b15a9bbc8ed20
odinokun <odinokun@gmail.com>
2017-05-23 19:07:17 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  // begin require SVG for IE
+  require('./modules/svg4everybody')();
+  // end   require SVG for IE
+
+  // begin Chrome Smooth Scroll
+  require('./modules/smoothScroll')();
+  // end   Chrome Smooth Scroll
+
+});
